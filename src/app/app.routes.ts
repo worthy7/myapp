2f34e8d5076ac967d38d87920efbae35423f24bc
Ian Worthington <worthy.vii@gmail.com>
2025-03-06 23:26:37 +0900

    showcase using SSG without a server by telling firebase to use index.csr.html directly and not use app-hosting but just normal hosting.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        
+        path: 'child',
+        loadComponent: () => import('./child/child.component').then(m => m.ChildComponent)
+    },
+    {
+        path: 'childa',
+        loadComponent: () => import('./child-a/child-a.component').then(m => m.ChildAComponent)
+    }
+];
---
+++ src/app/child-a/child-a.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-a',
+  imports: [],
+  templateUrl: './child-a.component.html',
+  styleUrl: './child-a.component.scss'
+})
+export class ChildAComponent {
+
+}
